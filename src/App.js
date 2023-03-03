c2fea896fedede5c68227e7459f2b2a3b7f1a9a6
SNKTV <45987846@gmail.com>
2023-03-03 21:37:53 +0200

    js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hello from "./componets/Hello.js";
+function App() {
+    return (
+        <div style={{ backgroundColor: '#46B1DE'}} className="wrapper">
+            <Hello></Hello>
+            <button onClick={() => console.log('Click on button')}>Button</button>
+        </div>
+    );
+}
+
+export default App;
